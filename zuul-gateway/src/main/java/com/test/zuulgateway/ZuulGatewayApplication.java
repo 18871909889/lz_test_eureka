ed2d483ca5cb620007d739458bb13896f930c326
lizhao <m18871909889@163.com>
2019-04-16 09:21:40 +0800

    添加eureka_client02的访问

---
+++ zuul-gateway/src/main/java/com/test/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.test.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//加上网关注解
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
